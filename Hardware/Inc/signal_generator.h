453a5fd8c19c75ad9df68bc6677f33799ee4c6a2
Ben Fox <b2k16@cinderhippo.com>
2021-09-01 12:58:00 -0400

    Signal generator driver

--- Hardware/Inc/signal_generator.h
+++ Hardware/Inc/signal_generator.h
@@ -9,5 +9,128 @@
 #ifndef INC_SIGNAL_GENERATOR_H_
 #define INC_SIGNAL_GENERATOR_H_
 
+#include <stdbool.h>
+#include "stm32f7xx_hal.h"
+
+typedef struct __attribute__((__packed__)) R0 {
+	uint8_t RESERVED : 1;
+	uint16_t INT : 16;
+	uint16_t FRAC : 12;
+	uint8_t CTRL : 3;
+} R0;
+
+typedef struct __attribute__((__packed__)) R1 {
+	uint8_t RESERVED : 4;
+	uint8_t PRESCALAR : 1;
+	uint16_t PHASE : 12;
+	uint16_t MOD : 12;
+	uint8_t CTRL : 3;
+} R1;
+
+typedef struct __attribute__((__packed__)) R2 {
+	uint8_t RESERVED : 1;
+	uint8_t LNLS : 2;  // Low-noise and low-spur modes
+	uint8_t MUXOUT : 3;
+	uint8_t RDOUBLER : 1; // Reference doubler
+	uint8_t RDIV2 : 1; // Reference divide-by-2
+	uint16_t RCOUNT : 10; // R counter
+	uint8_t DOUBLEBUFF : 1; // Double buffer
+	uint8_t ICP : 4; // Charge pump current setting
+	uint8_t LDF : 1;
+	uint8_t LDP : 1;
+	uint8_t PDPOLARITY : 1;
+	uint8_t POWERDOWN : 1;
+	uint8_t CP3STATE : 1;
+	uint8_t COUNTRESET : 1;
+	uint8_t CTRL : 3;
+} R2;
+
+typedef struct __attribute__((__packed__)) R3 {
+	uint16_t RESERVED : 13;
+	uint8_t CSR : 1; // Cycle slip reduction
+	uint8_t RESERVED2 : 1;
+	uint16_t CLKDIVMODE : 2; // Clock divider mode
+	uint16_t CLKDIVVAL : 12; // Clock divider value
+	uint8_t CTRL : 3;
+} R3;
+
+typedef struct __attribute__((__packed__)) R4 {
+	uint8_t RESERVED : 8;
+	uint8_t FBSELECT : 1; // Feedback select
+	uint8_t DIVSELECT : 3; // Divider select
+	uint16_t BANDSELCLKDIVVAL : 8; // Band select clock divider value
+	uint8_t VCOPOWERDOWN : 1;
+	uint8_t MTLD : 1; // Mute till lock detect
+	uint8_t AUXOUTSEL : 1; // Aux output select
+	uint8_t AUXOUTEN : 1; // Aux output enable
+	uint8_t AUXOUTPOW : 2; // Aux output power
+	uint8_t RFOUT : 1; // RF output enable
+	uint8_t POUT: 2; // Output power
+	uint8_t CTRL : 3;
+} R4;
+
+typedef struct __attribute__((__packed__)) R5 {
+	uint8_t RESERVED : 8;
+	uint8_t LDPIN : 2; // Lock detect pin operation
+	uint32_t RESERVED2 : 19;
+	uint8_t CTRL : 3;
+} R5;
+
+typedef union register_t {
+	R0 reg0;
+	R1 reg1;
+	R2 reg2;
+	R3 reg3;
+	R4 reg4;
+	R5 reg5;
+	uint32_t reg_vals[4];
+} register_t;
+
+typedef struct signal_generator_t {
+	SPI_HandleTypeDef* hspi;
+	GPIO_TypeDef* le_port;
+	uint16_t le_pin;
+	TIM_HandleTypeDef* htim;
+	uint32_t tim_channel;
+	GPIO_TypeDef* on_port;
+	uint16_t on_pin;
+	double ref_clk_freq_mhz;
+	bool is_init;
+	register_t regs[6];
+	double freq_pfd_mhz;
+} signal_generator_t;
+
+/**
+ * @brief Initialize signal generator. Must be called before any other functions.
+ * @param[out] dev: Signal generator device to hold properties
+ * @param[in] hspi: SPI handle for data
+ * @param[in] le_port: Latch enable GPIO port
+ * @param[in] le_pin: Latch enable GPIO pin
+ * @param[in] htim: Timer handle for reference clock
+ * @param[in] tim_channel: Timer channel for reference clock
+ * @param[in] on_port: Transmitter switch GPIO port
+ * @param[in] on_pin: Transmitter switch GPIO pin
+ * @param[in] ref_clk_freq_mhz: Frequency of the reference clock in MHz
+ */
+void signal_generator_init(signal_generator_t* dev, SPI_HandleTypeDef* hspi, GPIO_TypeDef* le_port, uint16_t le_pin, TIM_HandleTypeDef* htim, uint32_t tim_channel, GPIO_TypeDef* on_port, uint16_t on_pin, double ref_clk_freq_mhz);
+
+/**
+ * @brief Set the output frequency of the signal generator (137.5MHz - 4.4GHz)
+ * @param[in, out] dev: Signal generator device
+ * @param[in] freq_mhz: Frequency of output signal, at max resolution
+ */
+void signal_generator_set_output_freq(signal_generator_t* dev, double freq_mhz);
+
+/**
+ * @brief Start signal generation with current settings
+ * @param[in] dev: Signal generator device
+ */
+void signal_generator_start(const signal_generator_t* dev);
+
+/**
+ * @brief Stops signal generation
+ * @param[in] dev: Signal generator device
+ */
+void signal_generator_stop(const signal_generator_t* dev);
 
 #endif /* INC_SIGNAL_GENERATOR_H_ */
--- Hardware/Src/signal_generator.c
+++ Hardware/Src/signal_generator.c
@@ -4,3 +4,169 @@
 
 #include "signal_generator.h"
 
+#include <math.h>
+
+/**
+ * @brief Writes the given register based on the device struct values
+ * @param[in] dev: Signal generator device
+ * @param[in] register_num: Number of register to write (0-5)
+ */
+static void signal_generator_write_register(const signal_generator_t* dev, uint8_t register_num) {
+
+	// Check user inputs
+	if (!dev || register_num > 5) {
+		return;
+	}
+
+	// Produce register value (convert from little endian to big endian for SPI send)
+	uint8_t reg_vals[4] = {0};
+	for (int i = 0; i < 4; i++) {
+		reg_vals[i] = dev->regs[register_num].reg_vals[3 - i];
+	}
+
+	// SPI write to device
+	HAL_GPIO_WritePin(dev->le_port, dev->le_pin, GPIO_PIN_RESET);
+	HAL_SPI_Transmit(dev->hspi, reg_vals, 4, 100);
+	HAL_GPIO_WritePin(dev->le_port, dev->le_pin, GPIO_PIN_SET);
+}
+
+void signal_generator_init(signal_generator_t* dev, SPI_HandleTypeDef* hspi, GPIO_TypeDef* le_port, uint16_t le_pin, TIM_HandleTypeDef* htim, uint32_t tim_channel, GPIO_TypeDef* on_port, uint16_t on_pin, double ref_clk_freq_mhz) {
+
+	// Check user inputs
+	if (!dev || !hspi || !le_port || !htim || !on_port) {
+		return;
+	}
+
+	// Set dev properties
+	dev->hspi = hspi;
+	dev->le_port = le_port;
+	dev->le_pin = le_pin;
+	dev->on_port = on_port;
+	dev->on_pin = on_pin;
+	dev->htim = htim;
+	dev->tim_channel = tim_channel;
+	dev->ref_clk_freq_mhz = ref_clk_freq_mhz;
+	dev->is_init = false;
+
+	// Set register 0
+	R0* reg0 = &dev->regs[0].reg0;
+	reg0->RESERVED = 0;
+	reg0->INT = 0;
+	reg0->FRAC = 0;
+	reg0->CTRL = 0;
+
+	// Set register 1
+	R1* reg1 = &dev->regs[1].reg1;
+	reg1->RESERVED = 0;
+	reg1->PRESCALAR = 0;
+	reg1->PHASE = 1; // Recommended in datasheet
+	reg1->MOD = 4095; // Max resolution
+	reg1->CTRL = 1;
+
+	// Set register 2
+	R2* reg2 = &dev->regs[2].reg2;
+	reg2->LNLS = 0b11; // Low-spur mode
+	reg2->MUXOUT = 0b100; // N-divider output for debug
+	reg2->RDOUBLER = dev->ref_clk_freq_mhz < 16 ? 1 : 0;
+	reg2->RDIV2 = 0;
+	reg2->RCOUNT = (uint16_t) ceil(dev->ref_clk_freq_mhz * (1 + reg2->RDOUBLER) / 32.0);
+	reg2->DOUBLEBUFF = 0; // Disable, we have good enough control
+	reg2->ICP = 0b1000; // Average current (2.81mA)
+	reg2->LDF = 0; // Fractional lock detect
+	reg2->LDP = 0; // 40 10-ns cycles for lock detect
+	reg2->PDPOLARITY = 0; // Depends on loop filter
+	reg2->POWERDOWN = 0; // Keep on
+	reg2->CP3STATE = 0; // Normal operation
+	reg2->COUNTRESET = 0; // Normal operation
+	reg2->CTRL = 2;
+
+	// Set register 3
+	R3* reg3 = &dev->regs[3].reg3;
+	reg3->RESERVED = 0;
+	reg3->CSR = 0; // Disable cycle slip reduction
+	reg3->RESERVED2 = 0;
+	reg3->CLKDIVMODE = 0; // Phase clock off
+	reg3->CLKDIVVAL = 1; // No division
+	reg3->CTRL = 3;
+
+	// Set register 4
+	R4* reg4 = &dev->regs[4].reg4;
+	reg4->RESERVED = 0;
+	reg4->FBSELECT = 0; // Enable output dividers
+	reg4->DIVSELECT = 0;
+	reg4->BANDSELCLKDIVVAL = 1;
+	reg4->VCOPOWERDOWN = 0; // Keep VCO powered
+	reg4->MTLD = 0; // Disable mute till lock detect
+	reg4->AUXOUTSEL = 0; // Auxilary output from dividers
+	reg4->AUXOUTEN = 0; // Disable auxilary output
+	reg4->AUXOUTPOW = 0b11; // If aux enabled, output power max
+	reg4->RFOUT = 1; // Enable RF output
+	reg4->POUT = 0b11; // RF output power +5dBm
+	reg4->CTRL = 4;
+
+	// Set register 5
+	R5* reg5 = &dev->regs[5].reg5;
+	reg5->RESERVED = 0;
+	reg5->LDPIN = 0b01; // Digital lock detect as output
+	reg5->RESERVED2 = 0;
+	reg5->CTRL = 5;
+
+	// Calculate pfd frequency
+	// fpfd = REF_in * [(1 + D)/(R * (T + 1))]
+	// D = doubler bit (0 - 1)
+	// T = divide-by-2 bit (0 - 1)
+	// R = R counter divisor (1 - 1023)
+	dev->freq_pfd_mhz = ref_clk_freq_mhz * ((1 + reg2->RDOUBLER) / reg2->RCOUNT);
+
+	// Write registers
+	for (uint8_t i = 5; i >= 0; i++) {
+		signal_generator_write_register(dev, i);
+	}
+
+	// Turn on reference clock
+	HAL_TIM_OC_Start(dev->htim, dev->tim_channel);
+
+	// Turn off transmitter to start
+	HAL_GPIO_WritePin(dev->on_port, dev->on_pin, GPIO_PIN_RESET);
+}
+
+void signal_generator_set_output_freq(signal_generator_t* dev, double freq_mhz) {
+
+	// Check user inputs
+	if (!dev->is_init || freq_mhz < 137.5 || freq_mhz > 4400) {
+		return;
+	}
+
+	// Choose DIVSELECT (final divider either 1, 2, 4, 8, 16). VCO output is only 2.2GHz - 4.4GHz
+	// DIVSELECT = floor(log(4400/freq_mhz)/log(2))
+	dev->regs[4].reg4.DIVSELECT = (uint8_t) floor(log(4400.0 / freq_mhz) / log(2.0));
+	signal_generator_write_register(dev, 4);
+
+	// Set PLL values to produce desired output frequency
+	// Rf_out = fpfd * (INT + FRAC / MOD) / RF_div
+	// INT = (23 - 65535)
+	// MOD = (2 - 4095)
+	// FRAC = (0 - [MOD - 1])
+	// RF_div = DIVSELECT (1, 2, 4, 8, 16)
+	double remainder = freq_mhz * (double)(1u << dev->regs[4].reg4.DIVSELECT) / dev->freq_pfd_mhz;
+	double INT = floor(remainder);
+	double FRAC = (remainder - INT) * (double) dev->regs[0].reg1.MOD;
+	dev->regs[0].reg0.INT = (uint16_t) INT;
+	dev->regs[0].reg0.FRAC = (uint16_t) FRAC;
+	signal_generator_write_register(dev, 0);
+}
+
+void signal_generator_start(const signal_generator_t* dev) {
+	if (!dev->is_init)
+		return;
+
+	HAL_GPIO_WritePin(dev->on_port, dev->on_pin, GPIO_PIN_SET);
+}
+
+void signal_generator_stop(const signal_generator_t* dev) {
+	if (!dev->is_init)
+		return;
+
+	HAL_GPIO_WritePin(dev->on_port, dev->on_pin, GPIO_PIN_RESET);
+}
+
