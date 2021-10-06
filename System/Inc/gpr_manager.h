23659baa3e97a8d79cf4b6223a690aff5b962a45
Ben Fox <b2k16@cinderhippo.com>
2021-10-06 10:30:31 -0400

    Added frequency sweep to GPR manager

--- System/Inc/gpr_manager.h
+++ System/Inc/gpr_manager.h
@@ -15,18 +15,29 @@
 void gpr_manager_init();
 
 /**
- * @brief Sends GPR signal pulse at given frequency and start recording results
- * @param[in] freq_mhz: Frequency to send out pulse at in MHz
- * @param[in] num_samples: Number of samples to record
- * @param[in] pulse_time_us: Time of pulse in microseconds
+ * @brief Record GPR data in a step frequency sweep through the given frequency ranges
+ * @param[in] start_freq_mhz: Frequency to start sweep at in MHz
+ * @param[in] stop_freq_mhz: Frequency to stop sweep at
+ * @param[in] num_steps: Number of steps in the frequency sweep
+ * @param[in] num_samples_per_step: How long to record in each frequency step
  */
-void gpr_record_start(double freq_mhz, uint32_t num_samples, double pulse_time_us);
+void gpr_manager_start_recording(double start_freq_mhz, double stop_freq, int num_steps, int num_samples_per_step);
 
 /**
- * @brief Gets pointer to data from the GPR if recording isn't in progress
- * @param[out] num_samples: Number of samples retrieved from the GPR
- * @return Pointer to data if recording not in progress, NULL if recording is in progress
+ * @brief Continue the GPR manager recording
+ * @return True if GPR is still recording, false if GPR recording is complete
  */
-uint32_t* gpr_get_data(uint32_t* num_samples);
+bool gpr_manager_loop_recording();
+
+/**
+ * @brief Get data from GPR manager. Must be called while no recording is in progress to complete successfully.
+ * @param[out] data: Start address of data, in 2D array of length (num_steps, array_samples_per_step)
+ * @param[out] freqs_mhz: Frequencies (in MHz) matching up to each step in data
+ * @param[out] actual_num_steps: Number of steps in data. Also, equals # of rows in data and length of freqs_mhz
+ * @param[out] array_samples_per_step: Number of samples allocated in data array per step (length of row in data)
+ * @param[out] actual_samples_per_step: Number of samples actually used in each data step (# of usable columns in data)
+ * @return True if data retrieval was successful, False if failure (ie recording in progress)
+ */
+bool gpr_manager_get_data(uint32_t** data, double** freqs_mhz, int* actual_num_steps, int* array_samples_per_step, int* actual_samples_per_step);
 
 #endif /* INC_GPR_MANAGER_H_ */
