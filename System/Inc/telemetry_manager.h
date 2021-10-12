7812a6eccccaa63319df9c74d1c02958c6cb04ea
Ben Fox <b2k16@cinderhippo.com>
2021-10-12 00:01:52 -0400

    Telemetry manager radio queue management logic added

--- System/Inc/telemetry_manager.h
+++ System/Inc/telemetry_manager.h
@@ -8,6 +8,9 @@
 #ifndef INC_TELEMETRY_MANAGER_H_
 #define INC_TELEMETRY_MANAGER_H_
 
+#include <stdbool.h>
+#include <stdint.h>
+
 /**
  * @brief Initialize telemetry manager and its underlying hardware
  */
@@ -20,9 +23,10 @@ void telemetry_manager_init();
  * @param pos_z: Estimated position in meters of the robot center relative to its starting position along its up-down axis (up positive)
  * @param yaw: Estimated heading in degrees of the robot relative to its starting orientation in its x-y plane
  * @param roll: Estimated heading in degrees of the robot relative to its starting orientation in its x-z plane
- * @param pitch: Estimated heading in degrees of the robot relative to its starting orientation in its y-z plane
+ * @param pitch: Estimated heading in degrees of the robot relative to its starting orientation in its y-z plan
+ * @return Whether send was successfully queued (true) or not (false). Main cause of failure is full transmit queuee
  */
-void telemetry_manager_send_relative_pose(double pos_x, double pos_y, double pos_z, double yaw, double roll, double pitch);
+bool telemetry_manager_send_relative_pose(double pos_x, double pos_y, double pos_z, double yaw, double roll, double pitch);
 
 /**
  * @brief Telemeter estimate of initial, absolute robot pose
@@ -32,24 +36,26 @@ void telemetry_manager_send_relative_pose(double pos_x, double pos_y, double pos
  * @param yaw: Estimated starting heading in degrees of the robot in its longitude-latitude plane
  * @param roll: Estimated starting heading in degrees of the robot in its longitude-elevation plane
  * @param pitch: Estimated starting heading in degrees of the robot in its latitude-elevation plane
+ * @return Whether send was successfully queued (true) or not (false). Main cause of failure is full transmit queue
  */
-void telemetry_manager_send_absolute_pose(double longitude, double latitude, double elevation, double yaw, double roll, double pitch);
+bool telemetry_manager_send_absolute_pose(double longitude, double latitude, double elevation, double yaw, double roll, double pitch);
 
 /**
- * @brief Telemeter GPR data at a specific frequency
+ * @brief Telemeter GPR data at a specific frequency. This will maintain state unless restart parameter is passed or all data is completely sent
  * @param transmit_freq: Frequency in MHz of the signal that the GPR transmitter sent
  * @param mixer_ref_freq: Frequency in MHz of the reference signal the mixer was given to combine with the received signal
  * @param data_values: ADC inputs from the receiver
  * @param data_len: Number of samples in data
+ * @param restart: Whether the next incoming data is from a new set (true) or a continuation of an unfinished old set (false)
+ * @return Whether send was successfully queued (true) or not (false). Main cause of failure is full transmit queue
  */
-void telemetry_manager_send_gpr_data(double transmit_freq, double mixer_ref_freq, uint32_t* data_values, uint16_t data_len);
+bool telemetry_manager_send_gpr_data(double transmit_freq, double mixer_ref_freq, uint32_t* data_values, uint16_t data_len, bool restart);
 
 /**
  * @brief Telemeter data that helps monitor the robot
  * @param battery_voltage: Voltage of battery
+ * @return Whether send was successfully queued (true) or not (false). Main cause of failure is full transmit queue
  */
-void telemetry_manager_send_monitoring_data(double battery_voltage);\
-
-bool telemetry_manager_ready_to_send();
+bool telemetry_manager_send_monitoring_data(double battery_voltage);\
 
 #endif /* INC_TELEMETRY_MANAGER_H_ */
--- System/Src/telemetry_manager.c
+++ System/Src/telemetry_manager.c
@@ -2,8 +2,9 @@
  * telemetry_manager.c
  */
 
-#include "radio.h"
+#include "telemetry_manager.h"
 
+#include "radio.h"
 #include "peripheral_assigner.h"
 
 typedef enum {
@@ -38,12 +39,25 @@ static struct monitoring_payload_t {
 } monitoring_payload;
 
 static radio_t radio;
+static int cur_transmit_queue_size;
+static uint32_t last_transmit_time_ms;
+
+static void telemetry_manager_update_queue_size() {
+	cur_transmit_queue_size -= RADIO_TRANSMIT_SPEED_BPS * (HAL_GetTick() - last_transmit_time_ms) / 1000;
+	if (cur_transmit_queue_size < 0) {
+		cur_transmit_queue_size = 0;
+	}
+	last_transmit_time_ms = HAL_GetTick();
+}
 
 void telemetry_manager_init() {
 	radio_init(&radio, RADIO_UART);
 }
 
-void telemetry_manager_send_relative_pose(double pos_x, double pos_y, double pos_z, double yaw, double roll, double pitch) {
+bool telemetry_manager_send_relative_pose(double pos_x, double pos_y, double pos_z, double yaw, double roll, double pitch) {
+	// Update estimated queue size
+	telemetry_manager_update_queue_size();
+
 	// Set message header
 	message_header.message_id = RelativePose;
 	message_header.payload_len = sizeof(pose_payload);
@@ -54,13 +68,26 @@ void telemetry_manager_send_relative_pose(double pos_x, double pos_y, double pos
 	pose_payload.yaw = (float) yaw;
 	pose_payload.roll = (float) roll;
 	pose_payload.pitch = (float) pitch;
+
+	// Check if we can transmit into queue
+	uint16_t transmit_len = sizeof(message_header) + message_header.payload_len;
+	if (cur_transmit_queue_size + transmit_len > RADIO_QUEUE_SIZE) {
+		return false;
+	}
+
 	// Transmit message header
 	radio_transmit(&radio, (uint8_t*) &message_header, sizeof(message_header));
 	// Transmit message payload
 	radio_transmit(&radio, (uint8_t*) &pose_payload, sizeof(pose_payload));
+	cur_transmit_queue_size += transmit_len;
+
+	return true;
 }
 
-void telemetry_manager_send_absolute_pose(double longitude, double latitude, double elevation, double yaw, double roll, double pitch) {
+bool telemetry_manager_send_absolute_pose(double longitude, double latitude, double elevation, double yaw, double roll, double pitch) {
+	// Update estimated queue size
+	telemetry_manager_update_queue_size();
+
 	// Set message header
 	message_header.message_id = AbsolutePose;
 	message_header.payload_len = sizeof(pose_payload);
@@ -71,35 +98,95 @@ void telemetry_manager_send_absolute_pose(double longitude, double latitude, dou
 	pose_payload.yaw = (float) yaw;
 	pose_payload.roll = (float) roll;
 	pose_payload.pitch = (float) pitch;
+
+	// Check if we can transmit into queue
+	uint16_t transmit_len = sizeof(message_header) + message_header.payload_len;
+	if (cur_transmit_queue_size + transmit_len > RADIO_QUEUE_SIZE) {
+		return false;
+	}
+
 	// Transmit message header
 	radio_transmit(&radio, (uint8_t*) &message_header, sizeof(message_header));
 	// Transmit message payload
 	radio_transmit(&radio, (uint8_t*) &pose_payload, sizeof(pose_payload));
+	cur_transmit_queue_size += transmit_len;
+	return true;
 }
 
-void telemetry_manager_send_gpr_data(double transmit_freq, double mixer_ref_freq, uint32_t* data_values, uint16_t data_len) {
-	// Set message header
-	message_header.message_id = GPR;
-	message_header.payload_len = sizeof(gpr_payload) + data_len * sizeof(uint32_t); // 4 = size of uint32_t
-	// Set message payload
-	gpr_payload.transmit_freq = (float) transmit_freq;
-	gpr_payload.mixer_ref_freq = (float) mixer_ref_freq;
-	// Transmit message header
-	radio_transmit(&radio, (uint8_t*) &message_header, sizeof(message_header));
-	// Transmit message payload
-	radio_transmit(&radio, (uint8_t*) &gpr_payload, sizeof(gpr_payload));
-	// Transmit GPR data
-	radio_transmit(&radio, (uint8_t*) &data_values, data_len * sizeof(uint32_t));
+bool telemetry_manager_send_gpr_data(double transmit_freq, double mixer_ref_freq, uint32_t* data_values, uint16_t data_len, bool restart) {
+
+	// Set up internal state for continuing/restarting long data transmissions
+	static int next_data_byte_idx = 0;
+	static bool header_sent = false;
+	if (restart) {
+		next_data_byte_idx = 0;
+		header_sent = false;
+	}
+
+	// Update estimated queue size
+	telemetry_manager_update_queue_size();
+
+	if (!header_sent) {
+		// Set message header
+		message_header.message_id = GPR;
+		message_header.payload_len = sizeof(gpr_payload) + data_len * sizeof(uint32_t); // 4 = size of uint32_t
+		// Set message payload
+		gpr_payload.transmit_freq = (float) transmit_freq;
+		gpr_payload.mixer_ref_freq = (float) mixer_ref_freq;
+
+		// Check if we can transmit header/beginning of payload into queue
+		uint16_t transmit_len = sizeof(message_header) + sizeof(gpr_payload);
+		if (cur_transmit_queue_size + transmit_len > RADIO_QUEUE_SIZE) {
+			return false;
+		}
+
+		// Transmit message header
+		radio_transmit(&radio, (uint8_t*) &message_header, sizeof(message_header));
+		// Transmit message payload beginning
+		radio_transmit(&radio, (uint8_t*) &gpr_payload, sizeof(gpr_payload));
+		cur_transmit_queue_size += transmit_len;
+		header_sent = true;
+	}
+	if (header_sent) {
+		// Check how many bytes we can put into queue
+		uint16_t transmit_len = RADIO_QUEUE_SIZE - cur_transmit_queue_size;
+		bool should_stop = false;
+		if (transmit_len + next_data_byte_idx > data_len * sizeof(uint32_t)) {
+			transmit_len = data_len * sizeof(uint32_t) - next_data_byte_idx;
+			should_stop = true;
+		}
+
+		// Transmit data
+		radio_transmit(&radio, (uint8_t*) (data_values + next_data_byte_idx), transmit_len);
+		cur_transmit_queue_size += transmit_len;
+		next_data_byte_idx += transmit_len;
+		return should_stop;
+	}
+
+	// Return here should only occur if heading wasn't sent
+	return false;
 }
 
-void telemetry_manager_send_monitoring_data(double battery_voltage) {
+bool telemetry_manager_send_monitoring_data(double battery_voltage) {
+	// Update estimated queue size
+	telemetry_manager_update_queue_size();
+
 	// Set message header
 	message_header.message_id = Monitoring;
 	message_header.payload_len = sizeof(monitoring_payload);
 	// Set message payload
 	monitoring_payload.battery_voltage = (float) battery_voltage;
+
+	// Check if we can transmit into queue
+	uint16_t transmit_len = sizeof(message_header) + message_header.payload_len;
+	if (cur_transmit_queue_size + transmit_len > RADIO_QUEUE_SIZE) {
+		return false;
+	}
+
 	// Transmit message header
 	radio_transmit(&radio, (uint8_t*) &message_header, sizeof(message_header));
 	// Transmit message payload
 	radio_transmit(&radio, (uint8_t*) &monitoring_payload, sizeof(monitoring_payload));
+	cur_transmit_queue_size += transmit_len;
+	return true;
 }
