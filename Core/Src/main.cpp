fe91ea70c009cd6476f9fae1e5606143820f5065
Ben Fox <b2k16@cinderhippo.com>
2021-10-28 16:39:17 -0400

    Removed generated ethernet code causing unrecoverable error

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -21,7 +21,6 @@
 #include "main.h"
 #include "adc.h"
 #include "dma.h"
-#include "eth.h"
 #include "i2c.h"
 #include "spi.h"
 #include "tim.h"
@@ -94,7 +93,6 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_DMA_Init();
-  MX_ETH_Init();
   MX_USART3_UART_Init();
   MX_USB_OTG_FS_PCD_Init();
   MX_SPI3_Init();
