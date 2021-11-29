9119d2879c08092ff1252729a50a36a9342c76b8
Ben Fox <b2k16@cinderhippo.com>
2021-11-29 17:17:20 -0500

    Initialization state implemented

--- System/States/Src/state_initialize.cpp
+++ System/States/Src/state_initialize.cpp
@@ -4,14 +4,21 @@
 
 #include "state_initialize.h"
 
-void InitializeState::init() {
+#include "drive_manager.h"
+#include "gpr_manager.h"
+#include "sensor_manager.h"
+#include "telemetry_manager.h"
 
+void InitializeState::init() {
+	// Initialize all the managers, which in turn initialize the hardware
+	drive_manager_init();
+	gpr_manager_init();
+	sensor_manager_init();
+	telemetry_manager_init();
 }
 
 end_status_t InitializeState::run() {
-	return end_status_t::NoChange;
+	return end_status_t::InitializationComplete;
 }
 
-void InitializeState::cleanup() {
-
-}
+void InitializeState::cleanup() {}
